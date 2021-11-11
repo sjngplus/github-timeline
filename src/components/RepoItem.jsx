9f0e86b50f9cd916bbf5f6dd9107ab0acd75c54c
SJ <sj.jega2@gmail.com>
2021-11-11 16:52:58 -0500

    parse and display repo name and time

---
+++ src/components/RepoItem.jsx
@@ -0,0 +1,10 @@
+
+const RepoItem = (props) => {
+  return (
+    <div>
+      {props.repoName} | {props.time}
+    </div>
+  )
+}
+
+export default RepoItem
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,23 +1,35 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import RepoItem from './RepoItem';
 
 
 const Timeline = () => {
 
+  const [ repos, setRepos] = useState([]);
+  const gitHubUserName = "sjngplus";
+  
   useEffect(() => {
-    const gitHubUserName = "sjngplus";
     const url = `https://api.github.com/users/${gitHubUserName}/repos`;
-
+    console.log("##Fetching data from API##");
     axios.get(url)
-    .then(res => console.log(res.data))
+    .then(res => setRepos(res.data))
     .catch(err => console.log(err));
-  }, [])
+  }, [gitHubUserName])
 
+  const parsedReposList = repos.map(repo => {
+    const parsedName = repo.full_name.replace(`${gitHubUserName}/`, "");
+
+    return <RepoItem 
+      key={repo.id}
+      repoName={parsedName}
+      time={repo.created_at}
+    />
+  })
 
 
   return (
     <div>
-      Test
+      {parsedReposList}
     </div>
   )
 }
