c13b1ada60c32509ecc9700f98eb9283212bdd18
SJ <sj.jega2@gmail.com>
2021-11-16 18:35:47 -0500

    install and sass. Show unique icons depending on language used in repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Timeline from './components/Timeline';
 
 function App() {
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import "./VerticalTimeline.css"
+import "./VerticalTimeline.scss"
 import { BsGithub } from 'react-icons/bs';
+import { DiRuby } from 'react-icons/di';
+import { IoLogoJavascript, IoLogoHtml5, IoLogoPython } from 'react-icons/io';
 
 const Timeline = () => {
 
@@ -26,10 +28,21 @@ const Timeline = () => {
     .catch(err => console.log(err));
   }, [gitHubUserName]) 
   
+  const iconLogo = (language) => {
+    const icons = {
+      "JavaScript": <IoLogoJavascript />,
+      "HTML": <IoLogoHtml5 />,
+      "Ruby": <DiRuby />,
+      "Python": <IoLogoPython />,
+      "default": <BsGithub />
+    }
+    return icons[language] || icons.default;
+  };
   
   const parsedReposList = repos.map(repo => {
     const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
     const parsedRepoDate = new Date(repo.created_at).toLocaleDateString("en-US", dateOptions);
+    
     return  (
       <VerticalTimelineElement
         key={repo.id}
@@ -38,7 +51,7 @@ const Timeline = () => {
         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'grey', color: '#fff', }}
-        icon={<BsGithub />}       
+        icon={iconLogo(repo.language)}       
       >
       <a href={repo.html_url} target="_blank" rel="noopener noreferrer" style={{color: "inherit", textDecoration: "inherit"}}>
         <h3 className="vertical-timeline-element-title">{repo.name}</h3>
