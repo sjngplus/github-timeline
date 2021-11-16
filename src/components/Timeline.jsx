184a0df18f8e3b8ea5714ffb022454fe4fb9a845
SJ <sj.jega2@gmail.com>
2021-11-16 16:22:06 -0500

    Sort git hub repos according to date

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,8 @@
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import { BsGithub } from 'react-icons/bs';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import RepoItem from './RepoItem';
-
 
 const Timeline = () => {
 
@@ -14,31 +13,34 @@ const Timeline = () => {
     const url = `https://api.github.com/users/${gitHubUserName}/repos`;
     console.log("##Fetching data from API##");
     axios.get(url)
-    .then(res => setRepos(res.data))
+    .then(res => {
+      const responseDataArray = res.data;
+      responseDataArray.sort((a, b) => {
+        if (a.created_at < b.created_at) return -1;
+        if (a.created_at > b.created_at) return 1;
+        return 0;      
+      });
+      setRepos(responseDataArray);
+    })
     .catch(err => console.log(err));
-  }, [gitHubUserName])
-
-  // const parsedReposList = repos.map(repo => {
-  //   const parsedName = repo.full_name.replace(`${gitHubUserName}/`, "");
-  //   return <RepoItem 
-  //     key={repo.id}
-  //     repoName={parsedName}
-  //     time={repo.created_at}
-  //   />
-  // })
+  }, [gitHubUserName])  
+  
+  
   const parsedReposList = repos.map(repo => {
     const parsedRepoName = repo.full_name.replace(`${gitHubUserName}/`, "");
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const parsedRepoDate = new Date(repo.created_at).toLocaleDateString("en-US", dateOptions);
     return  (
       <VerticalTimelineElement
-        className="vertical-timeline-element--work"
+      className="vertical-timeline-element--work"
         contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
         contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
         date="2011 - present"
         iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        
+        icon={<BsGithub />}
       >
-        <h3 className="vertical-timeline-element-title">Creative Director</h3>
-        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <h3 className="vertical-timeline-element-title">{parsedRepoName}</h3>
+        <h5 className="vertical-timeline-element-subtitle">{parsedRepoDate}</h5>
         <p>
           Creative Direction, User Experience, Visual Design, Project Management, Team Leading
         </p>
@@ -48,9 +50,9 @@ const Timeline = () => {
 
 
   return (
-    <VerticalTimeline> 
+    <VerticalTimeline>
       {parsedReposList}
-    </VerticalTimeline> 
+    </VerticalTimeline>
   )
 }
 
