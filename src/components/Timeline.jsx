61e6c1160f7683cf921aea565a2bf8623ef90c73
SJ <sj.jega2@gmail.com>
2021-11-16 15:19:13 -0500

    Add vertical timeline component

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,3 +1,5 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import RepoItem from './RepoItem';
@@ -16,21 +18,39 @@ const Timeline = () => {
     .catch(err => console.log(err));
   }, [gitHubUserName])
 
+  // const parsedReposList = repos.map(repo => {
+  //   const parsedName = repo.full_name.replace(`${gitHubUserName}/`, "");
+  //   return <RepoItem 
+  //     key={repo.id}
+  //     repoName={parsedName}
+  //     time={repo.created_at}
+  //   />
+  // })
   const parsedReposList = repos.map(repo => {
-    const parsedName = repo.full_name.replace(`${gitHubUserName}/`, "");
-
-    return <RepoItem 
-      key={repo.id}
-      repoName={parsedName}
-      time={repo.created_at}
-    />
+    const parsedRepoName = repo.full_name.replace(`${gitHubUserName}/`, "");
+    return  (
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+        date="2011 - present"
+        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        
+      >
+        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+        </p>
+      </VerticalTimelineElement>
+    )
   })
 
 
   return (
-    <div>
+    <VerticalTimeline> 
       {parsedReposList}
-    </div>
+    </VerticalTimeline> 
   )
 }
 
